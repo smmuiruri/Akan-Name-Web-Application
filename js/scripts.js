51a3dac603858112c9d2d0fc91c657f22177ef5b
samwel mwaura <smmuiruri@gmail.com>
2020-05-29 09:46:09 -0700

    link bootstrap.css and styles.css to html

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,13 @@ let DD = parseInt(prompt("DD"))
 let MM = parseInt(prompt("MM"))
 let CC = parseInt(prompt("CC"))
 let YY = parseInt(prompt("YY"))
+let gender = prompt("gender")
 let name = (DD,MM,CC,YY) => {
     let results = ( ( parseInt(CC/4) -2*CC-1) + (parseInt(5*YY/4) ) + (parseInt(26*(MM+1)/10)) + DD ) % 7;
-    if (results === 0)
-    alert("SUNDAY")
-    else if(results === 1)
-    alert("MONDAY")
-    else if(results === 2)
-    alert("TUESDAY")
-    else if(results === 3)
-    alert("WEDNESDAY")
-    else if(results === 4)
-    alert("THURSDAY")
-    else if(results === 5)
-    alert("FRIDAY")
-    else if(results === 6)
-    alert("SATURDAY")
+    if (results === 0 && male)
+    alert("SUNDAY"); else alert("SONDAY");
+    if(results === 1&& male)
+    alert("MONDAY"); else alert ("MONDAY")
+
 }
 name(DD,MM,CC,YY);
