fd38d09cfd46f2243ea6f99b8aa77083f2e6a886
samwel mwaura <smmuiruri@gmail.com>
2020-05-31 08:38:52 -0700

    create string array of the malenames and femalenames

--- js/scripts.js
+++ js/scripts.js
@@ -14,4 +14,7 @@ function getDay() {
 }
 console.log(getDay());
 
+let malenames = ["Kwasi","Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let femalenames = ["Akosua","Adwoa", "Abenaa","Akua","Yaa", "Afua","Ama" ]
+
  
