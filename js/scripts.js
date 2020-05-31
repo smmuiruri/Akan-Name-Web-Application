1da1043acbc14e8cc79d5019dbace64ad21aeea2
samwel mwaura <smmuiruri@gmail.com>
2020-05-31 09:21:54 -0700

    create gender output for each case scenario depending on getDay output for the daynumber

--- js/scripts.js
+++ js/scripts.js
@@ -16,5 +16,67 @@ console.log(getDay());
 
 let malenames = ["Kwasi","Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let femalenames = ["Akosua","Adwoa", "Abenaa","Akua","Yaa", "Afua","Ama" ]
+let day = getday();
+function genderConfirm(){
+    var gender = document.getElementsByName("radio")
+    if (gender[0].checked == "true"){
+        var sex = male;
+    }else if (gender[1].checked == "true"){
+        var sex = female;
+    }else{
+        console.log("pass")        
+    }
+switch(sex){
+   case sex = "male":
+       switch(day){
+           case (0):
+           document.getElementById("result").innerHTML = "Your Akan Name is" + malenames[0];
+           document.getElementById("SUBMIT").innerHTML = "Your Akan Name is" + malenames[0];
+           case (1):
+           document.getElementById("result").innerHTML = "Your Akan Name is" + malenames[1];
+           document.getElementById("SUBMIT").innerHTML = "Your Akan Name is" + malenames[1];
+           case (2):
+           document.getElementById("result").innerHTML = "Your Akan Name is" + malenames[2];
+           document.getElementById("SUBMIT").innerHTML = "Your Akan Name is" + malenames[2];
+           case (3):
+           document.getElementById("result").innerHTML = "Your Akan Name is" + malenames[3];
+           document.getElementById("SUBMIT").innerHTML = "Your Akan Name is" + malenames[3];         
+           case (4):
+           document.getElementById("result").innerHTML = "Your Akan Name is" + malenames[4];
+           document.getElementById("SUBMIT").innerHTML = "Your Akan Name is" + malenames[4];
+           case (5):
+           document.getElementById("result").innerHTML = "Your Akan Name is" + malenames[5];
+           document.getElementById("SUBMIT").innerHTML = "Your Akan Name is" + malenames[5];
+           case (6):
+           document.getElementById("result").innerHTML = "Your Akan Name is" + malenames[6];
+           document.getElementById("SUBMIT").innerHTML = "Your Akan Name is" + malenames[6];              
+        }
+    case sex = "female":
+        switch(day){
+           case (0):
+           document.getElementById("result").innerHTML = "Your Akan Name is" + malenames[0];
+           document.getElementById("SUBMIT").innerHTML = "Your Akan Name is" + malenames[0];
+           case (1):
+           document.getElementById("result").innerHTML = "Your Akan Name is" + malenames[1];
+           document.getElementById("SUBMIT").innerHTML = "Your Akan Name is" + malenames[1];
+           case (2):
+           document.getElementById("result").innerHTML = "Your Akan Name is" + malenames[2];
+           document.getElementById("SUBMIT").innerHTML = "Your Akan Name is" + malenames[2];
+           case (3):
+           document.getElementById("result").innerHTML = "Your Akan Name is" + malenames[3];
+           document.getElementById("SUBMIT").innerHTML = "Your Akan Name is" + malenames[3];         
+           case (4):
+           document.getElementById("result").innerHTML = "Your Akan Name is" + malenames[4];
+           document.getElementById("SUBMIT").innerHTML = "Your Akan Name is" + malenames[4];
+           case (5):
+           document.getElementById("result").innerHTML = "Your Akan Name is" + malenames[5];
+           document.getElementById("SUBMIT").innerHTML = "Your Akan Name is" + malenames[5];
+           case (6):
+           document.getElementById("result").innerHTML = "Your Akan Name is" + malenames[6];
+           document.getElementById("SUBMIT").innerHTML = "Your Akan Name is" + malenames[6];            
+
+        } 
+}
+}
 
  
